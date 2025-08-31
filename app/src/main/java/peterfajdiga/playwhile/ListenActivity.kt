4da777412f29b14c9f33e5e5e5a672c7f5d1a31f
Peter Fajdiga <peter.fajdiga@gmail.com>
2025-08-31 08:21:29 +0200

    create media session (wip)

--- app/src/main/java/peterfajdiga/playwhile/ListenActivity.kt
+++ app/src/main/java/peterfajdiga/playwhile/ListenActivity.kt
@@ -1,8 +1,11 @@
 package peterfajdiga.playwhile
 
 import android.media.MediaPlayer
+import android.media.session.MediaSession
+import android.media.session.PlaybackState
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -30,6 +33,34 @@ class ListenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val mediaSession = MediaSession(this, "MyMediaSession").apply {
+            setCallback(object : MediaSession.Callback() {
+                override fun onPlay() {
+                    Log.d("MediaSession", "Play pressed")
+                }
+
+                override fun onPause() {
+                    Log.d("MediaSession", "Pause pressed")
+                }
+
+                override fun onSkipToPrevious() {
+                    Log.d("MediaSession", "Skip to Previous pressed")
+                }
+            })
+            isActive = true
+        }
+
+        val playbackState = PlaybackState.Builder()
+            .setActions(
+                PlaybackState.ACTION_PLAY or
+                PlaybackState.ACTION_PAUSE or
+                PlaybackState.ACTION_SKIP_TO_PREVIOUS
+            )
+            .setState(PlaybackState.STATE_PAUSED, 0L, 1.0f)
+            .build()
+        mediaSession.setPlaybackState(playbackState)
+
         val audioUri: Uri? = intent?.data
         setContent {
             PlayWhileTheme {
