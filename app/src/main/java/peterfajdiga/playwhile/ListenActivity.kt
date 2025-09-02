c054c965419d17907d45501b58de1d9b2f7e5c6b
Peter Fajdiga <peter.fajdiga@gmail.com>
2025-09-02 21:59:48 +0200

    enlarge UI in AudioPlayerScreen

--- app/src/main/java/peterfajdiga/playwhile/ListenActivity.kt
+++ app/src/main/java/peterfajdiga/playwhile/ListenActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -30,7 +31,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
@@ -60,10 +63,16 @@ class ListenActivity : ComponentActivity() {
 
 @Composable
 fun AudioPlayerScreen(audioUri: Uri, modifier: Modifier = Modifier) {
-    Box(modifier = modifier
-        .padding(16.dp)
-        .fillMaxSize()) {
-        AudioPlayerControls(audioUri, modifier = Modifier.align(Alignment.Center))
+    CompositionLocalProvider(
+        LocalDensity provides Density(
+            density = LocalDensity.current.density * 2.0f,
+        )
+    ) {
+        Box(modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()) {
+            AudioPlayerControls(audioUri, modifier = Modifier.align(Alignment.Center))
+        }
     }
 }
 
